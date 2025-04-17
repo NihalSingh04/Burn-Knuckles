e6f17cb69530d7bf1fda3453c01057f79e84f2ec
NihalSingh04 <nihalstudy2004@gmail.com>
2025-04-17 16:44:31 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // very important for React components!
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
